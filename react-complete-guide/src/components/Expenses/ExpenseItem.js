e95352b2a4eba61eb5d0d5519464229b47b67e29
sarniel <sarnielmorgia@gmail.com>
2022-04-21 12:05:43 +1000

    adding state

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+    const [name, setName] = useState(props.name);
+
+    const clickHandler = () => {
+        setName("Voalaahh!!!!");
+    }
+    
     return (
         <Card className="expense-item">
             <div>
                 <ExpenseDate date={props.date} name={props.name} amount={props.amount}/>
             </div>
-            <div className='expense-item_description'>
-                <h2>{props.name}</h2>
-                <div className='expense-item_price'>{props.amount}</div>
+            <div className='expense-item__description'>
+                <h2>{name}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
